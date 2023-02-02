7fc2f17f22b472e7629f4f780a6bdf1316bcd90b
Pilar Leiva Izquierdo <you@example.com>
2023-02-02 11:18:58 +0100

    variables 2.2

--- js/main.js
+++ js/main.js
@@ -6,24 +6,27 @@
 
 const catList = document.querySelector(".js-list");
 
+const kittenImage = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName = 'Anastacio';
+const kittenDesc = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenRace = 'Siamés';
+
 const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
-    alt="gatito"
-  />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>
-</article>
+<img 
+  class="card_img" 
+  src="${kittenImage}" 
+  alt="siames-cat"
+/>
+<h3 class="card_title> ${kittenName}</h3>
+<h4 class="card_race"> ${kittenRace}</h4>
+<p class="card_description"> ${kittenDesc}</p>
 </li>`;
 
-const kittenTwo = '<li class="card">
+
+catList.innerHTML = kittenOne;
+console.log (catList);
+
+const kittenTwo = `<li class="card">
 <img
   class="card_img"
   src="https://dev.adalab.es/sphynx-gato.webp"
@@ -36,9 +39,11 @@ const kittenTwo = '<li class="card">
   hasta con pinta de alienígena han llegado a definir a esta raza
   gatuna que se caracteriza por la «ausencia» de pelo.
 </p>
-</li>';
+</li>`;
 
-const kittenThree = '<li class="card">
+catList.innerHTML += kittenTwo;
+
+const kittenThree = `<li class="card">
 <img
   class="card_img"
   src="https://dev.adalab.es/maine-coon-cat.webp"
@@ -51,4 +56,6 @@ const kittenThree = '<li class="card">
   bella mirada se ha convertido en una de sus señas de identidad.
   Sus ojos son grandes y las orejas resultan largas y en punta.
 </p>
-</li>';
+</li>`;
+
+catList.innerHTML += kittenThree;
